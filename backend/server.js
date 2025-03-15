182509a06812568291c12082b59c9f9a7e89f081
mashrur-rahman-fahim <mashrur950@gmail.com>
2025-03-16 00:43:23 +0600

    check

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin:process.env.FRONTEND_URL,
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
