0e4e65962f7882fc6c165b0ab757db3284431446
mashrur-rahman-fahim <mashrur950@gmail.com>
2025-03-15 22:24:38 +0600

    error fixing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { DashboardPage } from './pages/DashboardPage'
+import { TestPage } from './pages/TestPage'
 
 export const App = () => {
   return (
@@ -9,6 +10,7 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/dashboard" element={<DashboardPage/>}/>
+      <Route path="/test" element={<TestPage/>}/>
     </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/TestPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Test } from '../components/test/Test'
+
+export const TestPage = () => {
+  return (
+    <div><Test/></div>
+  )
+}
