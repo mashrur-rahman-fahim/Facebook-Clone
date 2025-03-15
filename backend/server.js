a4874c470e324efbe48b857361568638e5e5e4a3
mashrur-rahman-fahim <mashrur950@gmail.com>
2025-03-16 00:50:29 +0600

    cors policy fixed

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,14 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin:process.env.FRONTEND_URL,
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL, // Allow requests from this origin
+    credentials: true, // Allow cookies and credentials
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
+  };
+  
+  app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
