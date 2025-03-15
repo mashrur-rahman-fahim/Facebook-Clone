dd92568a5b310407fe4d0bbd3ba84c3bd70523e1
mashrur-rahman-fahim <mashrur950@gmail.com>
2025-03-16 02:03:29 +0600

    check

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,17 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-const clientURLs = process.env.FRONTEND_URL.split(",");
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credential:true,
-}))
+// const clientURLs = process.env.FRONTEND_URL.split(",");
+const corsOptions = {
+  origin:process.env.FRONTEND_URL,
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 // const corsOptions = {
 //   origin: (origin, callback) => {
-//     if (clientURLs.indexOf(origin) !== -1 || !origin) {
+//     // console.log(clientURLs.indexOf(origin));
+//     if (true || !origin) {
 //       callback(null, true); // Allow the request
 //     } else {
 //       callback(new Error("Not allowed by CORS")); // Block the request
