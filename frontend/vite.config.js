937e3628893d2030ae86daa9cce1b76c7ecb0e4f
mashrur-rahman-fahim <mashrur950@gmail.com>
2025-03-15 23:12:04 +0600

    Deploy Vite frontend to Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Output directory for the build
-    emptyOutDir: true, // Clear the output directory before building
-  },
-  base: '/', // Set the base path for your app (use '/' for root)
-});
+  server:{
+    port: 3000
+  }
+})
