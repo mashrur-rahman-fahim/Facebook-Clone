ebcc2cb58ef9251bb752416d0700102b2d40d770
mashrur-rahman-fahim <mashrur950@gmail.com>
2025-03-16 01:28:54 +0600

    cors policy fixed

--- backend/server.js
+++ backend/server.js
@@ -11,22 +11,23 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 5000;
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}))
 
+// const clientURLs = process.env.FRONTEND_URL.split(",");
 
-
-  
-const clientURLs = process.env.FRONTEND_URL.split(',');
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (clientURLs.indexOf(origin) !== -1 || !origin) {
-      callback(null, true); // Allow the request
-    } else {
-      callback(new Error('Not allowed by CORS')); // Block the request
-    }
-  },
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-};
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (clientURLs.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true); // Allow the request
+//     } else {
+//       callback(new Error("Not allowed by CORS")); // Block the request
+//     }
+//   },
+//   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+// };
 
 app.use(cors(corsOptions));
 
@@ -38,9 +39,9 @@ connectDB();
 app.use("/auth", authRoutes);
 app.use("/api", apiRoutes);
 
-app.get('/',(req,res)=>{
-    return res.send("hello");
-})
+app.get("/", (req, res) => {
+  return res.send("hello");
+});
 app.listen(port, () => {
   console.log(`Server is running `);
 });
