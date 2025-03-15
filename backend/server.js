17b71b50a95b73c98636abefdec0b4b26cca37e7
mashrur-rahman-fahim <mashrur950@gmail.com>
2025-03-16 01:02:37 +0600

    cors policy fixed

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 
 const corsOptions = {
-    origin: "*", // Allow requests from this origin
+    origin: "https://faceloop.vercel.app", // Allow requests from this origin
     credentials: true, // Allow cookies and credentials
     allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
   };
